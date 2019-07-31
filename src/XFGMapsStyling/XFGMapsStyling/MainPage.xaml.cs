fd38fd5174d49b2642a4fb9eb87a6a85acd9ffa0
Alexsandre Almeida <aralmeida@mpes.mp.br>
2019-07-31 19:19:07 -0300

    initial project with Shiny.Locations, used for Geofencing, and Xamarin Forms Google Maps

---
+++ src/XFGMapsStyling/XFGMapsStyling/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.GoogleMaps;
+
+namespace XFGMapsStyling
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //var assembly = this.GetType().Assembly;
+            //Stream stream = assembly.GetManifestResourceStream("XFGMapsStyling.Resources.MapStylLes.Dark.json");
+            //string json;
+            //using (var reader = new StreamReader(stream))
+            //{
+            //    json = reader.ReadToEnd();
+            //}
+
+            //GMap.MapStyle = MapStyle.FromJson(json);
+        }
+    }
+}
